30f42bebbf438936b7980cd7e3ef65c75d91b596
Ashish <ashish@a.patel7734@gmail.com>
2014-08-16 08:45:03 -0700

    finishing basic tipster

--- Tipster/ViewController.swift
+++ Tipster/ViewController.swift
@@ -9,17 +9,43 @@
 import UIKit
 
 class ViewController: UIViewController {
-                            
+    
+    @IBOutlet var billTextField: UITextField!
+    @IBOutlet var tipLabel: UILabel!
+    @IBOutlet var totalLabel: UILabel!
+    @IBOutlet var percentageSC: UISegmentedControl!
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        tipLabel.text="$0.00"
+        totalLabel.text = "$0.00"
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    @IBAction func onBillAmountEditingChanged(sender: AnyObject) {
+        var tipPercentages = [0.18,0.2,0.22]
+        var tipPercentage = tipPercentages[        percentageSC.selectedSegmentIndex]
+        var billAmntStr = NSString(string: billTextField.text);
+        var billAmount = billAmntStr.doubleValue;
+        var tipAmount = billAmount * tipPercentage;
+        var total = billAmount + tipAmount
+        tipLabel.text=String(format: "%.2f",tipAmount)
+        totalLabel.text=String(format: "%.2f", total)
+        
+    }
+    
+    @IBAction func onTap(sender: AnyObject) {
+        view.endEditing(true)
+    }
+    
 }
 
